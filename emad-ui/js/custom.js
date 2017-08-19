6976660cab98ac8e618af7572bd304179d9b07b6
Negar Jamalifard <n.jamalifard@gmail.com>
2017-08-19 12:26:02 +0430

    emad-ui index animation
    header deleted from index page
    nav relocated under the line
    animations effect added

--- emad-ui/js/custom.js
+++ emad-ui/js/custom.js
@@ -4,4 +4,21 @@ jQuery(function($){
        $('.nav-list').slideToggle();
     });
 
+    $('.load-line').animate({
+        width:"100%"
+    },1500,titleFadeIn);
+
+    function titleFadeIn() {
+        $('.title-big').delay(500).animate({
+            opacity :1
+        },1500, slideFade);
+    }
+    function slideFade() {
+        $('.nav-center').animate({
+            opacity:1,
+            marginTop:0,
+            marginBottom:20
+        },800);
+    }
+
 }); //End Of siaf!
