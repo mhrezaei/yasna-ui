3cf321a4b166618d6ab5007be3823ee2babf6eb6
Negar Jamalifard <n.jamalifard@gmail.com>
2017-08-23 10:28:50 +0430

    slider func relocated to new file
    slider related functions relocated to a new js file.

--- emad-ui/js/custom.js
+++ emad-ui/js/custom.js
@@ -22,16 +22,7 @@ jQuery(function($){
         },800);
     }
 
-    $('.img-row').owlCarousel({
-        items:30,
-        margin:0,
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:1000,
-        autoplayHoverPause:true,
-        nav:false,
-        autoWidth:true
-    });
+
 
 
 
