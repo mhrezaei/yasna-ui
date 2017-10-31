5748c2d5f38755a54f8755df647175c0a070e348
Negar Jamalifard <n.jamalifard@gmail.com>
2017-10-31 12:31:02 +0330

    navbar responsive and slider navigation btns
    - navbar in header responsiveness
    - sliders have arrow on bigger screens

--- payment/public/assets/js/general.js
+++ payment/public/assets/js/general.js
@@ -1,10 +1,14 @@
 /*
+*----------------------------------------------------------------------
 * Owl Carousel Setup
+* ---------------------------------------------------------------------
 * */
 jQuery(function($){
 
     // Banner Carousel
     $('#header-slider').owlCarousel({
+        nav:true,
+        navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
         loop: true,
         rtl: true,
         responsive:{
@@ -23,6 +27,8 @@ jQuery(function($){
     // Services Carousel
     $('#service-slider').owlCarousel({
         items: 1,
+        nav:true,
+        navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
         loop: true,
         rtl: true,
         center:true,
@@ -30,4 +36,27 @@ jQuery(function($){
         startPosition: 'URLHash'
     })
 
+}); //End Owl Carousel Setup
+
+/*
+ *----------------------------------------------------------------------
+ * Control Buttons
+ * ---------------------------------------------------------------------
+ * */
+jQuery(function($){
+
+    var ToggleBTN = $('.header .js-hamburger-btn'),
+        nav = $('.header .js-hamburger-nav');
+
+    ToggleBTN.on('click',function (e) {
+        e.preventDefault();
+        nav.slideToggle();
+    })
+
 }); //End Of siaf!
+
+/*
+ *----------------------------------------------------------------------
+ * Modernizer
+ * ---------------------------------------------------------------------
+ * */
