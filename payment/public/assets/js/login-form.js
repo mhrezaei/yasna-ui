880cdbccf9482ad5824538936fc638c4d6fa23dd
Negar Jamalifard <n.jamalifard@gmail.com>
2017-11-07 17:23:15 +0330

    register part and password strength checking
    -register added to layout and selectable through top tabs
    - strength of password will be checked by external plugin

--- payment/public/assets/js/login-form.js
+++ payment/public/assets/js/login-form.js
@@ -9,31 +9,36 @@
      var tabs = $('.js-tabs-control'),
          tabsLeft = tabs.offset().left,
          tab = $('.js-tabs-control .login-tabs__tab'),
-         passInput = $('#password'),
-         passHidden = ( passInput.attr('type') === 'password' ),
          input = $('.form-input input');
 
+    //onload functions
+     moveUnderline();
+     changeForm();
 
+
+    //changes content according to the selected tab
      tab.on('click',function (e) {
          e.preventDefault();
          var $this = $(this),
-             href = $this.children('a').attr('href'),
-             lineLeft = $this.offset().left - tabsLeft;
+             href = $this.children('a').attr('href');
 
          $('.js-tabs-control .active').removeClass('active');
          $this.addClass('active');
 
-         $('.login-tabs-underline').animate({
-             left: lineLeft
-         });
-
+         moveUnderline();
+         changeForm();
      });
 
 
      // Toggle password view
      $('.js-toggle-visibility').on('click',function (e) {
          e.preventDefault();
-         var $this = $(this);
+         var $this = $(this),
+             id = "#" + $this.data('passinput'),
+             passInput = $(id),
+             passHidden = ( passInput.attr('type') === 'password' );
+
+
         if(passHidden){
             passInput.attr('type','text');
             $this.children('i.fa')
@@ -49,6 +54,7 @@
         }
      });
 
+     //changes input class if filled
      input.on('change', function () {
          var $this = $(this);
          if( $this.val().length ){
@@ -58,6 +64,109 @@
          }
      });
 
+
+
+     //Moves the tab's underline, under the active tab
+     function moveUnderline() {
+
+         var lineLeft = tabs.children('.active').offset().left - tabsLeft;
+
+         $('.login-tabs-underline').css({
+             left: lineLeft
+         });
+     }
+     //shows relared form
+     function changeForm() {
+         var activeTab = tabs.children('.active'),
+             tabTarget = activeTab.data('target');
+
+         $('div[class$="-content"]').hide();
+         $('.'+ tabTarget +'-content').fadeIn();
+     }
+
+ }); //End Of siaf!
+
+/*
+*-------------------------------------------------------
+* Form resetting
+*-------------------------------------------------------
+*/
+jQuery(function($){
+
+    // Disable scrolling on input number
+    $(':input[type=number]').on('mousewheel', function(e){
+        e.preventDefault();
+    });
+
+}); //End Of siaf!
+
+/*
+*-------------------------------------------------------
+* Password stregth check
+*-------------------------------------------------------
+*/
+
+ jQuery(function($){
+
+     "use strict";
+     var lang = $('html').attr('lang'),
+         options = {};
+     options.ui = {
+         container: "#pwd-container",
+         //showVerdictsInsideProgressBar: false,
+         viewports: {
+             progress: ".pw-progress",
+             verdict: ".pw-verdict"
+         },
+         colorClasses: ["danger", "danger", "warning", "warning", "success", "success"],
+         progressBarExtraCssClasses: "progress-bar-striped active"
+     };
+     options.common = {
+         onKeyUp: function () {
+             translateToFa();
+         }
+
+     };
+
+     $('#reg-password').pwstrength(options);
+
+
+         function translateToFa (){
+             if(lang==="fa") {
+                 var verdictContainer = $('#pwd-container .password-verdict'),
+                     text = verdictContainer.text(),
+                     translation;
+
+
+                 switch (text) {
+                     case "Very Weak":
+                         translation = "بسیار ضعیف";
+                         break;
+                     case "Weak":
+                         translation = "ضعیف";
+                         break;
+                     case "Normal":
+                         translation = "متوسط";
+                         break;
+                     case "Medium":
+                         translation = "خوب";
+                         break;
+                     case "Strong":
+                         translation = "قوی";
+                         break;
+                     case "Very Strong":
+                         translation = "بسیار قوی";
+                         break;
+                     default:
+                         translation = "";
+
+
+                 }
+                 verdictContainer.text(translation);
+             }else{
+                 return
+             }
+         }
 
 
 
